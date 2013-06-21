5724fece3b112fdf7d82a1783778d97fe51307a3
unknown <thomasneirynck@hotmail.com>
2013-06-21 11:40:54 +0200

    lazy init of props in Evented/Promise

---
+++ testsrc/dev/RTree.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomasn
+ * Date: 12/06/13
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ testsrc/dev/douglasPeucker.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomasn
+ * Date: 5/06/13
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ */
